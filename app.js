95b4598afc7f2c8e18b08617d1723d0553e90e5d
MathildeDumazet <31658369+MathildeDumazet@users.noreply.github.com>
2017-10-24 17:30:55 +0200

    session8

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
 var app = new Vue({
   el: '#app',
   data: {
-    firstName: "Jules",
-    lastName: "Bonnard",
-    description: "Je suis datajournaliste à l'AFP et j'enseigne le code, le traitement, et la visualisation de données à Sciences Po.",
-    photo: "https://pbs.twimg.com/profile_images/899285838516936704/nfeLD7he_400x400.jpg",
-    twitter: "julesbonnard",
-    github: "julesbonnard",
-    mail: "julesbonnard@gmail.com",
+    firstName: "Mathilde",
+    lastName: "Dumazet",
+    description: "Je suis étudiante en journalisme à Sciences Po.",
+    photo: "https://media-exp1.licdn.com/mpr/mpr/shrinknp_200_200/p/4/005/0ac/295/068f4bf.jpg",
+    twitter: "MathildeDumazet",
+    github: "MathildeDumazet",
+    mail: "mathilde.dumazet@gmail.com",
     experiences: [{
-      dateBegin: "2014",
-      dateEnd: "Today",
-      name: "Agence France Presse",
-      title: "Datajournalist",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    }, {
-      dateBegin: "Été 2013",
+      dateBegin: "July 2017",
       dateEnd: false,
-      name: "Le Monde.fr",
-      title: "Data-journalist",
-      description: false
+      name: "Le Monde",
+      title: "Journaliste stagiaire au service culture",
+      logo: 'http://www.sppef.fr/wp-content/uploads/2017/10/sppef_logo-le-monde-sites-monuments.png',
+      description: "Production de contenus éditoriaux",
     }, {
-      dateBegin: "2012",
-      dateEnd: "2013",
-      name: "Le Dauphiné Libéré",
-      title: "Stagiaire et correspondant de presse",
-      description: false
+      dateBegin: "May 2017",
+      dateEnd: "June 2017",
+      name: "Slate.fr",
+      title: "Journaliste stagiaire",
+      logo: 'http://www.slate.fr/sites/all/themes/slate/images/fb_placeholder.jpg',
+      description: "Production de contenus éditoriaux",
+    }, {
+      dateBegin: "March 2016",
+      dateEnd: "April 2016",
+      name: "Little White Lies",
+      title: "Journaliste stagiaire et critique cinéma",
+      logo: 'https://pbs.twimg.com/profile_images/688056455660974080/lq8r5IbC.jpg',
+      description: false,
     }],
     formations: [{
-      dateBegin: "2012",
-      dateEnd: "2014",
-      name: "Master Journalisme",
-      university: "Sciences Po Grenoble"
+      dateBegin: "2016",
+      dateEnd: "2018",
+      name: "Master de Journalisme",
+      university: "Sciences Po Paris"
     }, {
-      dateBegin: "2009",
-      dateEnd: "2012",
-      name: "Bachelor Sciences Politiques",
-      university: "Sciences Po Grenoble"
+      dateBegin: "2013",
+      dateEnd: "2016",
+      name: "Bachelor Sciences sociales",
+      university: "Sciences Po Paris",
     }],
     languages: ["Anglais"],
-    skills: ["Node", "VueJS", "D3js", "SCSS", "HTML"]
+    skills: ["Indesign", "Social networks", "Cultural policies", "HTML"]
   },
   computed: {
     fullName() {
